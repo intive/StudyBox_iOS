12f9400ec6e28c9de7b992f1476f7b8e0b0d3786
Piotr Tobolski <piotr.tobolski@blstream.com>
2016-02-27 16:02:15 +0100

    Add CocoaPods

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'StudyBox_iOS' do
+    pod 'MMDrawerController', '~> 0.6.0'
+end
+
+target 'StudyBox_iOSTests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-StudyBox_iOSTests/Pods-StudyBox_iOSTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_StudyBox_iOSTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_StudyBox_iOSTestsVersionString[];
+
