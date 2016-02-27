60d341dcf57392b4a45ee7deee4ab4081f37b3d0
Piotr Tobolski <piotr.tobolski@blstream.com>
2016-02-27 15:24:29 +0100

    Setup project

---
+++ StudyBox_iOSTests/StudyBox_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  StudyBox_iOSTests.swift
+//  StudyBox_iOSTests
+//
+//  Created by Piotr Tobolski on 27.02.2016.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import XCTest
+@testable import StudyBox_iOS
+
+class StudyBox_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
