e96c07e1f40bdde2cceda5740035eb3f25da27dd
Ymcyk <thepateto@gmail.com>
2016-03-07 16:33:32 +0100

    View Controller partially implemented
    Added UIControllerViewDelegate and UICollectionViewDataSource. CollectionView shows cells. After tapped cell, TestView is pushed.
    Labels in cells use fonts from extension.
    Without DataModel and Colours.

--- StudyBox_iOS/DecksViewCell.swift
+++ StudyBox_iOS/DecksViewCell.swift
@@ -10,4 +10,6 @@ import UIKit
 
 class DecksViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var deckNameLabel: UILabel!
+    
 }
--- StudyBox_iOS/DecksViewController.swift
+++ StudyBox_iOS/DecksViewController.swift
@@ -8,6 +8,63 @@
 
 import UIKit
 
-class DecksViewController: StudyBoxViewController {
-
+class DecksViewController: StudyBoxViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    // czeka na DataModel
+    // private var decksArray: [Deck]?
+    @IBOutlet var decksCollectionView: UICollectionView!
+    
+    override func viewDidAppear(animated: Bool) {
+        // czeka na DataModel
+        // Coś w stylu: decksArray = Deck.loadDecks().map{$0 < $1}
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        decksCollectionView.delegate = self
+        decksCollectionView.dataSource = self
+        // tutaj zmiana koloru
+        // view.backgroundColor = UIColor.redColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // czeka na DataModel
+        // return decksArray.count
+        return 5
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("DecksViewCellID", forIndexPath: indexPath)
+            as! DecksViewCell
+        // czeka na DataModel
+        // Coś w stylu: cell.deckNameLabel.text = decksArray[indexPath.row].name
+        cell.deckNameLabel.text = "Przykład"
+        
+        return cell
+    }
+    
+    // MARK: Change view to test
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // ustawianie wybranej talii
+        // let selectedDeck = decksArray[indexPath.row]
+        // Coś w stylu: TestViewController.actualTest = selectedDeck
+        let testView = storyboard?.instantiateViewControllerWithIdentifier("TestViewControllerID")
+        let testNavigation = UINavigationController(rootViewController: testView!)
+        self.presentViewController(testNavigation, animated: true, completion: nil)
+    }
+    
+    func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("DecksViewCellID", forIndexPath: indexPath)
+            as! DecksViewCell
+        cell.deckNameLabel.font = UIFont.studyBoxBlack()
+        // czeka na Kolory
+        // cell.backgroundColor = UIColor.redColor() - niestety takie coś nie działa, trzeba innego sposobu na zmianę koloru komórki
+    }
 }
