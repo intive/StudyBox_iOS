934c54d97af50cf7e655b5e0612c2a1d42b28f21
Ymcyk <thepateto@gmail.com>
2016-03-08 12:57:15 +0100

    Now view is changed by performSegueWithIdentifier (push StartTest)
    Added StartTest push segue from DeckView to TestView
    Added performSegueWithIdetifier and prepareForSegue method
    Deleted presentViewController from didSelectItemAtIndexPath

--- StudyBox_iOS/DecksViewController.swift
+++ StudyBox_iOS/DecksViewController.swift
@@ -22,14 +22,22 @@ class DecksViewController: StudyBoxViewController, UICollectionViewDelegate, UIC
         super.viewDidLoad()
         decksCollectionView.delegate = self
         decksCollectionView.dataSource = self
-        // tutaj zmiana koloru
-        // view.backgroundColor = UIColor.redColor()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
+    // Before segue do this
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "StartTest" {
+            if let _ = segue.destinationViewController as? TestViewController {
+                // destinationTestView.currentDeckForTesting = selectedDeck
+            }
+        }
+    }
+    
+    // Populate cells
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
@@ -50,21 +58,19 @@ class DecksViewController: StudyBoxViewController, UICollectionViewDelegate, UIC
         return cell
     }
     
-    // MARK: Change view to test
+    // When cell tapped, change to test
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         // ustawianie wybranej talii
         // let selectedDeck = decksArray[indexPath.row]
         // Coś w stylu: TestViewController.actualTest = selectedDeck
-        let testView = storyboard?.instantiateViewControllerWithIdentifier("TestViewControllerID")
-        let testNavigation = UINavigationController(rootViewController: testView!)
-        self.presentViewController(testNavigation, animated: true, completion: nil)
+        self.performSegueWithIdentifier("StartTest", sender: nil)
     }
     
+    // Change cell color and font
     func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("DecksViewCellID", forIndexPath: indexPath)
             as! DecksViewCell
         cell.deckNameLabel.font = UIFont.studyBoxBlack()
-        // czeka na Kolory
-        // cell.backgroundColor = UIColor.redColor() - niestety takie coś nie działa, trzeba innego sposobu na zmianę koloru komórki
+        // tu można jeszcze zmienić kolor
     }
 }
