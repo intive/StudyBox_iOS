23fdcd753555b0b6cd02743a5b1405bf2e1dab90
Kacper Czapp <kacper1703@gmail.com>
2016-03-09 09:44:34 +0100

    Reset DecksVC to avoid conflict

--- StudyBox_iOS/DecksViewController.swift
+++ StudyBox_iOS/DecksViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 
 class DecksViewController: StudyBoxViewController {
   
-// prawdopodobnie będzie potrzebne do przesłania danych między DeckView a TestView
-//  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//    if segue.identifier == "StartTest" {
-//      if let TestViewController = segue.destinationViewController as? TestViewController {
-//        TestViewController.currentDeckForTesting = sender as? Deck
-//      }
-//    }
-//  }
+
   
 }
