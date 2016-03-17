5f20e413c169424696ac0a1eacc61cbbdf8ce41c
Piotr Rudnicki <prudnicki@plog.pl>
2016-03-17 10:10:45 +0100

    poprawki

--- StudyBox_iOS/ScoreViewController.swift
+++ StudyBox_iOS/ScoreViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class ScoreViewController: StudyBoxViewController {
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var deckListButton: UIButton!
+    @IBOutlet weak var runTestButton: UIButton!
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        deckListButton.backgroundColor = UIColor(red: 0.88, green: 0.16, blue: 0.32, alpha: 1.0)
+        deckListButton.setTitleColor(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1), forState: UIControlState.Normal)
+        deckListButton.titleLabel?.font = UIFont.sbFont(bold: false)
+        
+        runTestButton.backgroundColor = UIColor(red: 0.88, green: 0.16, blue: 0.32, alpha: 1.0)
+        runTestButton.setTitleColor(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1), forState: UIControlState.Normal)
+        runTestButton.titleLabel?.font = UIFont.sbFont(bold: false)
         
         completeData()
     }
