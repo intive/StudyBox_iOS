589e613519bb8ca638d330147de27177400f5e0c
Piotr Tobolski <piotr.tobolski@blstream.com>
2016-03-24 09:49:18 +0100

    Fix xcpretty dependency

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -3,4 +3,5 @@ set -e
 
 brew update
 brew install xctool || brew outdated xctool || brew upgrade xctool
-brew install xcpretty || brew outdated xcpretty || brew upgrade xcpretty
+
+gem install xcpretty
