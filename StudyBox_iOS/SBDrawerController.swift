254042ca7a0ff7bb0b861bb8ea4f970a107f3702
Damian Malarczyk <damian.malarczyk@gmail.com>
2016-04-02 20:46:20 +0200

    attempt to fix status bar animation bug which breaks layout of current `centerViewController` from drawer

--- StudyBox_iOS/SBDrawerController.swift
+++ StudyBox_iOS/SBDrawerController.swift
@@ -8,47 +8,70 @@
 
 import UIKit
 import MMDrawerController
-protocol SBDrawerLeftDelegate {
-    func hidingDrawer()
-}
+
 protocol SBDrawerCenterDelegate {
-    func showingDrawer()
+    func drawerToggleAnimation()
 }
 
 class SBDrawerController:MMDrawerController {
-    var drawerDelegate:SBDrawerLeftDelegate?
     var centerDelegate:SBDrawerCenterDelegate?
     
-    static var statusBarAnimationTime = 0.5
+    static var statusBarAnimationTime = 0.25
     
-    override var centerViewController: UIViewController! {
-        didSet {
-            if let delegate = centerViewController as? SBDrawerCenterDelegate {
-                centerDelegate = delegate
-            }else {
-                centerDelegate = nil 
+    private func updateCenterDelegate() {
+        var sbController = self.centerViewController as? StudyBoxViewController
+        if sbController == nil {
+            if let navigationController = self.centerViewController as? UINavigationController where !navigationController.childViewControllers.isEmpty {
+                
+                sbController = navigationController.childViewControllers[0] as? StudyBoxViewController
             }
+        }
+        
+        if let delegate = sbController as? SBDrawerCenterDelegate {
+            centerDelegate = delegate
+        }else {
+            centerDelegate = nil
         }
     }
     
-    override var leftDrawerViewController: UIViewController! {
+    override func setCenterViewController(newCenterViewController: UIViewController!, withFullCloseAnimation fullCloseAnimated: Bool, completion: ((Bool) -> Void)!) {
+        super.setCenterViewController(newCenterViewController, withFullCloseAnimation: fullCloseAnimated, completion: completion)
+        updateCenterDelegate()
+    }
+    override func setCenterViewController(centerViewController: UIViewController!, withCloseAnimation closeAnimated: Bool, completion: ((Bool) -> Void)!) {
+        super.setCenterViewController(centerViewController, withCloseAnimation: closeAnimated, completion: completion)
+        updateCenterDelegate()
+    }
+    override var centerViewController: UIViewController! {
         didSet {
-            if let delegate = leftDrawerViewController as? SBDrawerLeftDelegate {
-                drawerDelegate = delegate 
-            }else {
-                drawerDelegate = nil
-            }
+            updateCenterDelegate()
+            
         }
     }
     
     override func closeDrawerAnimated(animated: Bool, velocity: CGFloat, animationOptions options: UIViewAnimationOptions, completion: ((Bool) -> Void)!) {
-        drawerDelegate?.hidingDrawer()
-        super.closeDrawerAnimated(animated, velocity: velocity, animationOptions: options, completion: completion)
+        var completionBlock = completion
+        
+        if let sbController = centerDelegate as? StudyBoxViewController {
+            sbController.isDrawerVisible = true
+            
+            if sbController.isViewLoaded() == false  {
+                sbController.setNeedsStatusBarAppearanceUpdate()
+            }
+            completionBlock = {[completion] (success:Bool) -> Void in
+                sbController.drawerToggleAnimation()
+                
+                completion?(success)
+            }
+            
+        }
+        
+        super.closeDrawerAnimated(animated, velocity: velocity, animationOptions: options, completion: completionBlock)
         
     }
     
     override func openDrawerSide(drawerSide: MMDrawerSide, animated: Bool, completion: ((Bool) -> Void)!) {
-        centerDelegate?.showingDrawer()
+        centerDelegate?.drawerToggleAnimation()
         super.openDrawerSide(drawerSide, animated: animated, completion: completion)
     }
     
