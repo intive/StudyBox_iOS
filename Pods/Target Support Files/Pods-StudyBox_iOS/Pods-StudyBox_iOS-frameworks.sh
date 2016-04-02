fd0065d88ed451e4b4e13a84b2ac990f7775f4ea
Kacper Czapp <kacper1703@gmail.com>
2016-04-02 22:36:00 +0200

    Remove KDCircularProgress pods + init file

--- Pods/Target Support Files/Pods-StudyBox_iOS/Pods-StudyBox_iOS-frameworks.sh
+++ Pods/Target Support Files/Pods-StudyBox_iOS/Pods-StudyBox_iOS-frameworks.sh
@@ -16,7 +16,7 @@ install_framework()
     local source="$1"
   fi
 
-  local destination="${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+  local destination="${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 
   if [ -L "${source}" ]; then
       echo "Symlinked..."
@@ -84,20 +84,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$CONFIGURATION_BUILD_DIR/Alamofire/Alamofire.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/KDCircularProgress/KDCircularProgress.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/MMDrawerController/MMDrawerController.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/Reachability/Reachability.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/Realm/Realm.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/RealmSwift/RealmSwift.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MMDrawerController/MMDrawerController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Reachability/Reachability.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$CONFIGURATION_BUILD_DIR/Alamofire/Alamofire.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/KDCircularProgress/KDCircularProgress.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/MMDrawerController/MMDrawerController.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/Reachability/Reachability.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/Realm/Realm.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/RealmSwift/RealmSwift.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MMDrawerController/MMDrawerController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Reachability/Reachability.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
