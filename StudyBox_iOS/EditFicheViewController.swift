7cba067a3c222724c7f64e53cb6e116b15b0a360
Damian Malarczyk <damian.malarczyk@gmail.com>
2016-04-02 20:42:34 +0200

    initial `EditFicheViewController` implementation

--- StudyBox_iOS/EditFicheViewController.swift
+++ StudyBox_iOS/EditFicheViewController.swift
@@ -8,6 +8,57 @@
 
 import UIKit
 
-class EditFicheViewController: StudyBoxViewController {
+enum EditFicheViewControllerMode {
+    case Add,Modify(deckName:String,flashcard:Flashcard)
+}
 
+class EditFicheViewController: StudyBoxViewController {
+    
+    @IBOutlet weak var deckField: UITextField!
+    @IBOutlet weak var questionField: UITextField!
+    @IBOutlet weak var tipField: UITextField!
+    @IBOutlet weak var answerField: UITextField!
+    private var flashcard:Flashcard?
+    private var deckName:String?
+    
+    var mode:EditFicheViewControllerMode? {
+        didSet {
+            if let editMode = mode {
+                switch editMode {
+                case .Modify(let deckName,let card):
+                    flashcard = card
+                    self.deckName = deckName
+                    
+                    deckField.text = deckName
+                    questionField.text = card.question
+                    tipField.text = card.tip
+                    answerField.text = card.answer
+                    navigationItem.title = "Edytuj"
+                case .Add:
+                    navigationItem.title = "Stwórz"
+                }
+            }else {
+                clearInput()
+                navigationItem.title = nil
+            }
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        assert(mode != nil, "Mode not choosen!")
+    }
+    
+    func clearInput(){
+        deckField.text = nil
+        questionField.text = nil
+        tipField.text = nil
+        answerField.text = nil
+
+    }
+
+    @IBAction func saveAction(sender: UIBarButtonItem) {
+        /// TODO send data to server
+    }
 }
