67835def9ebec73a6f99a9e8693c8932549e0e35
Damian Malarczyk <damian.malarczyk@gmail.com>
2016-04-08 12:31:03 +0200

    Add save, update logic

--- StudyBox_iOS/EditFlashcardViewController.swift
+++ StudyBox_iOS/EditFlashcardViewController.swift
@@ -18,36 +18,20 @@ class EditFlashcardViewController: StudyBoxViewController {
     @IBOutlet weak var questionField: UITextField!
     @IBOutlet weak var tipField: UITextField!
     @IBOutlet weak var answerField: UITextField!
-    private var flashcard:Flashcard?
+    
+    private var flashcard:Flashcard!
     private var deckName:String?
     
-    var mode:EditFlashcardViewControllerMode? {
+    var mode:EditFlashcardViewControllerMode! {
         didSet {
-            if let editMode = mode {
-                switch editMode {
-                case .Modify(let deckName,let card):
-                    flashcard = card
-                    self.deckName = deckName
-                    
-                    deckField.text = deckName
-                    questionField.text = card.question
-                    tipField.text = card.tip
-                    answerField.text = card.answer
-                    navigationItem.title = "Edytuj"
-                case .Add:
-                    navigationItem.title = "Stwórz"
-                }
-            }else {
-                clearInput()
-                navigationItem.title = nil
+            if isViewLoaded() {
+                updateUiForCurrentMode()
             }
         }
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        assert(mode != nil, "Mode not choosen!")
+    var dataManager:DataManager? {
+        return UIApplication.appDelegate().dataManager
     }
     
     func clearInput(){
@@ -55,10 +39,86 @@ class EditFlashcardViewController: StudyBoxViewController {
         questionField.text = nil
         tipField.text = nil
         answerField.text = nil
-
+        
     }
-
+    
+    private func updateUiForCurrentMode() {
+        clearInput()
+        
+        switch mode {
+        case .Modify(let deckName,let card)?:
+            flashcard = card
+            self.deckName = deckName
+            
+            deckField.text = deckName
+            questionField.text = card.question
+            tipField.text = card.tip
+            answerField.text = card.answer
+            navigationItem.title = "Edytuj"
+        case .Add?:
+            navigationItem.title = "Stwórz"
+            flashcard = nil
+        default:
+            navigationItem.title = nil
+            flashcard = nil
+            deckName = nil
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        assert(mode != nil, "mode not choosen!")
+        
+        updateUiForCurrentMode()
+    }
+    
     @IBAction func saveAction(sender: UIBarButtonItem) {
-        /// TODO send data to server
+        
+        guard let answer = answerField.text, question = questionField.text else {
+            presentAlertController(withTitle: "Błąd", message: "Pola odpowiedzi i pytania nie mogą być puste", buttonText: "Ok")
+            return
+        }
+        
+        var tip:Tip?
+        if let tipText = tipField.text {
+            tip = Tip.Text(text: tipText)
+        }
+        
+        if case .Add? = mode {
+            
+            if let deckFieldName = deckField.text {
+                
+                if let dataDeck = dataManager?.deck(withName: deckFieldName, caseSensitive: true) {
+                    do {
+                        try dataManager?.addFlashcard(forDeckWithId: dataDeck.id, question: question, answer: answer, tip: tip)
+                    } catch _ as DataManagerError {
+                        presentAlertController(withTitle: "Błąd", message: "Nie udało się dodać fiszki", buttonText: "Ok")
+                    } catch let err {
+                        print("EditFlashcardViewController adding error : \(err)")
+                    }
+                    
+                }
+
+            }
+        } else if case .Modify? = mode {
+            
+            flashcard.question = question
+            flashcard.answer = answer
+            flashcard.tipEnum = tip
+        
+            do {
+                try dataManager?.updateFlashcard(flashcard)
+            } catch _ as DataManagerError {
+                presentAlertController(withTitle: "Błąd", message: "Nie udało się zapisać zmian", buttonText: "Ok")
+            } catch let err {
+                print("EditFlashcardViewController update error : \(err)")
+            }
+                
+            
+        }
+        
+        
     }
 }
