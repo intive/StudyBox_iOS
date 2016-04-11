d17556c482b7f5eb8223ab9ba9c60b6b21a1085f
Kacper Czapp <kacper1703@gmail.com>
2016-04-11 17:36:44 +0200

    Added Watch target and app icons.

---
+++ StudyBox Watch App Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  StudyBox Watch App Extension
+//
+//  Created by Kacper Cz on 11.04.2016.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
