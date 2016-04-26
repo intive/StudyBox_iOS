9fbe7e09811e8ce260a7ac2d1902991fb2299f6b
Ymcyk <thepateto@gmail.com>
2016-04-26 12:02:36 +0200

    Removed unused setUp and tearDown methods

--- StudyBox_iOSTests/TestLogicTests.swift
+++ StudyBox_iOSTests/TestLogicTests.swift
@@ -10,16 +10,6 @@ import XCTest
 @testable import StudyBox_iOS
 
 class TestLogicTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        
-        super.tearDown()
-    }
 
     func testCheckIfAllFlashcardsHidden() {
         // checking if true
