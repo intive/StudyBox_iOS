3a17e6b3313cf870ac7fce3c321e3ab58ed74d54
Ymcyk <thepateto@gmail.com>
2016-04-26 17:49:24 +0200

    Divide tests into smaller functions. Groups
    added // prepare // do // check groups in all tests

--- StudyBox_iOSTests/TestLogicTests.swift
+++ StudyBox_iOSTests/TestLogicTests.swift
@@ -10,170 +10,211 @@ import XCTest
 @testable import StudyBox_iOS
 
 class TestLogicTests: XCTestCase {
-
-    func testCheckIfAllFlashcardsHidden() {
-        // checking if true
-        var mockData = MockData(amount: 1, hidden: 1)
-        var testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
+    
+    // number of flashcards in array. Bigger than one
+    let flashcardsNumber = 10
+    
+// checkIfAllFlashcardsHidden()
+    
+    func testCheckIfAllFlashcardsHiddenWithHiddenFlashcards() {
+        // prepare
+        let mockData = MockData(amount: flashcardsNumber, hidden: flashcardsNumber)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
         
-        XCTAssertTrue(testLogic.checkIfAllFlashcardsHidden(), "Initialized with 1 deck, Learn mode and all hidden, checkIfAllFlashcardsHidden should be true")
+        // do
+        let result = testLogic.checkIfAllFlashcardsHidden()
         
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(1))
-        
-        XCTAssertTrue(testLogic.checkIfAllFlashcardsHidden(), "Initialized with 1 deck, Test mode and all hidden, checkIfAllFlashcardsHidden should be true")
-        
-        mockData = MockData(amount: MockData.notOneNumber, hidden: MockData.notOneNumber)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
-        
-        XCTAssertTrue(testLogic.checkIfAllFlashcardsHidden(), "Initialized with \(MockData.notOneNumber) deck(s), Learn mode and all hidden, checkIfAllFlashcardsHidden should be true")
-        
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(1))
-        
-        XCTAssertTrue(testLogic.checkIfAllFlashcardsHidden(), "Initialized with \(MockData.notOneNumber) deck(s), Test mode and all hidden, checkIfAllFlashcardsHidden should be true")
-        
-        // checking if false
-        mockData = MockData(amount: 1, hidden: 0)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
-        
-        XCTAssertFalse(testLogic.checkIfAllFlashcardsHidden(), "Initialized with 1 deck(s), Learn mode and no hidden, checkIfAllFlashcardsHidden should be false")
-        
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(1))
-        
-        XCTAssertEqual(1, testLogic.cardsAnsweredAndPossible().1, "There should be only one card in test")
-        
-        XCTAssertFalse(testLogic.checkIfAllFlashcardsHidden(), "Initialized with 1 deck(s), Test mode and no hidden, checkIfAllFlashcardsHidden should be false")
-        
-        mockData = MockData(amount: MockData.notOneNumber, hidden: 1)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
-        
-        XCTAssertFalse(testLogic.checkIfAllFlashcardsHidden(), "Initialized with \(MockData.notOneNumber) deck(s), Learn mode and 1 hidden, checkIfAllFlashcardsHidden should be false")
-        
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(1))
-        
-        XCTAssertEqual(1, testLogic.cardsAnsweredAndPossible().1, "There should be only one card in test")
-        
-        XCTAssertFalse(testLogic.checkIfAllFlashcardsHidden(), "Initialized with \(MockData.notOneNumber) deck(s), Test mode and 1 hidden, checkIfAllFlashcardsHidden should be false")
+        // check
+        XCTAssertTrue(result, "Initialized with all hidden flashcards, method should return True")
     }
     
-    func testCheckIfPassedDeckIsEmpty() {
-        // checking if returning true
-        var testLogic = Test(deck: [], testType: .Learn)
+    func testCheckIfAllFlashcardsHiddenWithNoHiddenFlashcards() {
+        // prepare
+        let mockData = MockData(amount: flashcardsNumber)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
         
-        XCTAssertTrue(testLogic.checkIfPassedDeckIsEmpty(), "Initialized with empty Flashcard array and Learn mode, passedDeckWasEmpty should be true")
+        // do
+        let result = testLogic.checkIfAllFlashcardsHidden()
         
-        testLogic = Test(deck: [], testType: .Test(1))
-        
-        XCTAssertEqual(0, testLogic.cardsAnsweredAndPossible().1, "There should not be any card int test")
-        XCTAssertTrue(testLogic.checkIfPassedDeckIsEmpty(), "Initialized with empty Flashcard array and Test mode, passedDeckWasEmpty should be true")
-        
-        // checking if returning false
-        let mockData = MockData(amount: 1)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
-        
-        XCTAssertFalse(testLogic.checkIfPassedDeckIsEmpty(), "Initialized with not empty Flashcard array, passedDeckWasEmpty should be false")
-        
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(1))
-        
-        XCTAssertEqual(1, testLogic.cardsAnsweredAndPossible().1, "There should not be any card int test")
-        
-        XCTAssertFalse(testLogic.checkIfPassedDeckIsEmpty(), "Initialized with not empty Flashcard array and Test mode, passedDeckWasEmpty should be false")
+        // check
+        XCTAssertFalse(result, "Initialized with not hidden flashcards, method should return False")
     }
     
-    func testCorrectAnswer() {
-        // should return nil, empty array
-        var testLogic = Test(deck: [], testType: .Learn)
+// checkIfPassedDeckIsEmpty()
+    
+    func testCheckIfPassedDeckIsEmptyWithEmptyArray() {
+        // prepare
+        let testLogic = Test(deck: [], testType: .Learn)
         
-        XCTAssertNil(testLogic.correctAnswer(), "No flashcards in deck, correctAnswer should resturn nil")
-        XCTAssertEqual(testLogic.index, 1, "index should be increased, after correctAnswer")
+        // do
+        let result = testLogic.checkIfPassedDeckIsEmpty()
         
-        var mockData = MockData(amount: 1)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
+        // check
+        XCTAssertTrue(result, "Initialized with empty Flashcard array, passedDeckWasEmpty should be true")
+    }
+    
+    func testCheckIfPassedDeckIsEmptyWithNotEmptyArray() {
+        // prepare
+        let mockData = MockData(amount: flashcardsNumber)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
         
-        XCTAssertNil(testLogic.correctAnswer(), "Only one flashcard in deck, correctAnswer should resturn nil")
-        XCTAssertEqual(testLogic.index, 1, "index should be increased, after correctAnswer")
+        // do
+        let result = testLogic.checkIfPassedDeckIsEmpty()
         
-        // not nil
-        mockData = MockData(amount: MockData.notOneNumber)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
-        
-        XCTAssertNotNil(testLogic.correctAnswer(), "\(MockData.notOneNumber) flashcards in deck, correctAnswer should return not nil object")
-        XCTAssertEqual(testLogic.index, 1, "index should be increased, after correctAnswer")
-        
-        for _ in 2...MockData.notOneNumber {
-            testLogic.correctAnswer()
-        }
-        
-        XCTAssertEqual(testLogic.index, MockData.notOneNumber, "index should be equal \(MockData.notOneNumber)")
-        
-        // test from 10 decks
-        mockData = MockData(amount: 10)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(10))
-        
-        XCTAssertEqual(10, testLogic.cardsAnsweredAndPossible().1, "There should be 10 cards in test")
-        
-        for _ in 1...9 {
-            XCTAssertNotNil(testLogic.correctAnswer(), "Only last correctAnswer call should return nil")
-        }
-        
-        XCTAssertNil(testLogic.correctAnswer(), "Only last correctAnswer call should return nil")
-        
+        // check
+        XCTAssertFalse(result, "Initialized with not empty Flashcard array, passedDeckWasEmpty should be false")
+    }
+    
+// correctAnswer()
+    
+    func testCorrectAnswerWithNoFlashcards() {
+        // prepare
+        let testLogic = Test(deck: [], testType: .Learn)
         let passed = testLogic.cardsAnsweredAndPossible().0
         
-        XCTAssertEqual(passed, 10, "Passed cards should be equal 10")
+        // do
+        let result = testLogic.correctAnswer()
+        
+        // check
+        XCTAssertNil(result, "No flashcards in deck, correctAnswer should resturn nil in Learn mode")
+        XCTAssertTrue((testLogic.cardsAnsweredAndPossible().0 - passed) == 1, "passedFlashcard should be increased by one ")
     }
     
-    func testIncorrectAnswer() {
-        // should return nil, empty array
-        var testLogic = Test(deck: [], testType: .Learn)
+    func testCorrectAnswerWithOneFlashcard() {
+        // prepare
+        let mockData = MockData(amount: 1)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
+        let passed = testLogic.cardsAnsweredAndPossible().0
         
-        XCTAssertNil(testLogic.incorrectAnswer(), "No flashcards in deck, incorrectAnswer should resturn nil")
-        XCTAssertEqual(testLogic.index, 0, "index should not be increased, after incorrectAnswer in Learn mode")
+        // do
+        let result = testLogic.correctAnswer()
         
-        // one flashcard, Learn mode
-        var mockData = MockData(amount: 1)
-        let givenFlashcard = mockData.flashcardArray.last!
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
+        // check
+        XCTAssertNil(result, "One flashcard in deck, correctAnswer should resturn nil in Learn mode")
+        XCTAssertTrue((testLogic.cardsAnsweredAndPossible().0 - passed) == 1, "passedFlashcard should be increased by one ")
+    }
+    
+    func testCorrectAnswerWithMoreThanOneFlashcard() {
+        // prepare
+        let mockData = MockData(amount: flashcardsNumber)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
+        
+        // do
+        var result: Flashcard?
+        for _ in 1...flashcardsNumber-1 {
+            result = testLogic.correctAnswer()
+            XCTAssertNotNil(result, "All except last calls sshould return not nil object")
+        }
+        result = testLogic.correctAnswer()
+        
+        // check
+        XCTAssertNil(result, "One flashcard in deck, correctAnswer should return nil in Learn mode")
+        XCTAssertTrue(testLogic.cardsAnsweredAndPossible().0 == flashcardsNumber, "passedFlashcard should be increased by one ")
+    }
+    
+// incorrectAnswer()
+    
+    func testIncorrectAnswerWithNoFlashcards() {
+        // prepare
+        let testLogic = Test(deck: [], testType: .Learn)
+        
+        // do
         let result = testLogic.incorrectAnswer()
         
-        XCTAssertNotNil(result, "Deck not empty, incorrectAnswer should return next flashcard")
-        XCTAssertTrue(result === givenFlashcard, "One flashcard in deck, incorrectAnswer in Learn mode should return the same object")
-        
-        // few flashcards, Learn mode
-        mockData = MockData(amount: 2)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
-        
-        let second = testLogic.incorrectAnswer()
-        let first = testLogic.incorrectAnswer()
-        
-        XCTAssertNotNil(first, "Deck not empty, incorrectAnswer should return next flashcard")
-        XCTAssertFalse(first === second, "Two flashcards in deck, incorrectAnswer in Learn mode should return diffrent objects")
- 
-        // test from one flashcard, Test mode
-        mockData = MockData(amount: 1)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(1))
-        
-        XCTAssertNil(testLogic.incorrectAnswer(), "Test from one flashcard, incorrectAnswer should resturn nil")
-        
-        mockData = MockData(amount: MockData.notOneNumber)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Test(1))
-        
-        XCTAssertEqual(1, testLogic.cardsAnsweredAndPossible().1, "There should be only one card in test")
-        XCTAssertNil(testLogic.incorrectAnswer(), "Test from one flashcard, incorrectAnswer should resturn nil")
-
+        // check
+        XCTAssertNil(result, "No flashcards in deck, incorrectAnswer should return nil")
     }
     
-    func testSkipCard() {
-        // should return nil, empty array
-        var testLogic = Test(deck: [], testType: .Learn)
+    func testIncorrectAnswerWithOneFlashcardInLearnMode() {
+        // prepare
+        let flashcard = Flashcard(serverID: "serverID", deckId: "deckID", question: "question", answer: "answer", tip: Tip.Text(text: "Tip"))
+        let testLogic = Test(deck: [flashcard], testType: .Learn)
         
-        XCTAssertNil(testLogic.skipCard(), "No flashcards in deck, skipCard should resturn nil")
+        // do
+        let result = testLogic.incorrectAnswer()
         
-        // should return nil, empty array
-        let mockData = MockData(amount: 1)
-        testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
-        let skipped = testLogic.skipCard()
+        // check
+        XCTAssertTrue(result === flashcard, "One flashcard in deck, incorrectAnswer in Learn mode should return the same object")
+    }
+    
+    func testIncorrectAnswerWithTwoFlashcardsInLearnMode() {
+        // prepare
+        let mockData = MockData(amount: 2)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
         
-        XCTAssertTrue(skipped === testLogic.skipCard(), "No flashcards in deck, skipCard should resturn nil")
+        // do
+        let second = testLogic.incorrectAnswer()!
+        let first = testLogic.incorrectAnswer()!
+        
+        // check
+        XCTAssertFalse(first === second, "Two flashcards in deck, incorrectAnswer in Learn mode should return diffrent objects")
+    }
+    
+    func testIncorrectAnswerWithOneFlashcardInTestMode() {
+        // prepare
+        let flashcard = Flashcard(serverID: "serverID", deckId: "deckID", question: "question", answer: "answer", tip: Tip.Text(text: "Tip"))
+        let testLogic = Test(deck: [flashcard], testType: .Test(1))
+        
+        // do
+        let result = testLogic.incorrectAnswer()
+        
+        // check
+        XCTAssertNil(result, "One flashcard in deck, incorrectAnswer should return nil in Test Mode")
+    }
+    
+    func testIncorrectAnswerWithMoreThanOneFlashcardInTestMode() {
+        // prepare
+        let mockData = MockData(amount: flashcardsNumber)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Test(uint(flashcardsNumber)))
+        
+        // do
+        var result: Flashcard?
+        for _ in 1...flashcardsNumber-1 {
+            result = testLogic.incorrectAnswer()
+            XCTAssertNotNil(result, "All except last calls should return not nil object")
+        }
+        result = testLogic.incorrectAnswer()
+        
+        // check
+        XCTAssertNil(result, "Last incorrectAnswer from original deck should return nil")
+    }
+    
+// skipCard()
+    
+    func testSkipCardWithNoFlashcards() {
+        // prepare
+        let testLogic = Test(deck: [], testType: .Learn)
+        
+        // do
+        let result = testLogic.skipCard()
+        
+        // check
+        XCTAssertNil(result, "No flashcards in deck, skipCard should return nil")
+    }
+    
+    func testSkipCardWithOneFlashcard() {
+        // prepare
+        let flashcard = Flashcard(serverID: "serverID", deckId: "deckID", question: "question", answer: "answer", tip: Tip.Text(text: "Tip"))
+        let testLogic = Test(deck: [flashcard], testType: .Learn)
+        
+        // do
+        let result = testLogic.skipCard()!
+        
+        // check
+        XCTAssertNotNil(result, "One flashcard in deck, skipCard should return not nil object")
+        XCTAssertTrue(flashcard === result, "Returned flashcard should be the same object")
+    }
+    
+    func testSkipCardWithMoreThanOneFlashcard() {
+        // prepare
+        let mockData = MockData(amount: flashcardsNumber)
+        let testLogic = Test(deck: mockData.flashcardArray, testType: .Learn)
+        
+        // do
+        let result = testLogic.skipCard()
+        
+        // check
+        XCTAssertNotNil(result, "More than one flashcard in deck, skipCard should return not nil object")
     }
     
     class MockData {
