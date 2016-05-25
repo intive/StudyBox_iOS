5d084cfd5bf36206f7472942fc71a9f5eca6bc7f
Kacper Cz <kacper1703@gmail.com>
2016-05-25 18:00:21 +0200

    Change tint color

--- StudyBox_iOS/AppDelegate.swift
+++ StudyBox_iOS/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         Fabric.with([Crashlytics.self])
+
+        self.window?.tintColor = UIColor.sb_DarkBlue()
+        
         return true
     }
     
